593162979f3965c74faf7e1436cae8be38a1f0c7
PrasathSivalingam <sprasath@gmail.com>
2014-04-24 04:13:18 +0200

    iOS and android support added

--- AquaVet.Mobile/platforms/android/bin/classes.dex.d
+++ AquaVet.Mobile/platforms/android/bin/classes.dex.d
@@ -1,10 +1,9 @@
 /Users/oceabergen/Mercatus/AquaVet/Mobile/PhoneGap/AquaVet.Mobile/platforms/android/bin/classes.dex : \
-/Users/oceabergen/Mercatus/AquaVet/Mobile/PhoneGap/AquaVet.Mobile/platforms/android/bin/classes/com/example/www/AquaVet.class \
-/Users/oceabergen/Mercatus/AquaVet/Mobile/PhoneGap/AquaVet.Mobile/platforms/android/bin/classes/com/example/www/BuildConfig.class \
-/Users/oceabergen/Mercatus/AquaVet/Mobile/PhoneGap/AquaVet.Mobile/platforms/android/bin/classes/com/example/www/R$attr.class \
-/Users/oceabergen/Mercatus/AquaVet/Mobile/PhoneGap/AquaVet.Mobile/platforms/android/bin/classes/com/example/www/R$drawable.class \
-/Users/oceabergen/Mercatus/AquaVet/Mobile/PhoneGap/AquaVet.Mobile/platforms/android/bin/classes/com/example/www/R$string.class \
-/Users/oceabergen/Mercatus/AquaVet/Mobile/PhoneGap/AquaVet.Mobile/platforms/android/bin/classes/com/example/www/R$xml.class \
-/Users/oceabergen/Mercatus/AquaVet/Mobile/PhoneGap/AquaVet.Mobile/platforms/android/bin/classes/com/example/www/R.class \
 /Users/oceabergen/Mercatus/AquaVet/Mobile/PhoneGap/AquaVet.Mobile/platforms/android/bin/classes/no/ocea/aquavet/AquaVet.class \
+/Users/oceabergen/Mercatus/AquaVet/Mobile/PhoneGap/AquaVet.Mobile/platforms/android/bin/classes/no/ocea/aquavet/BuildConfig.class \
+/Users/oceabergen/Mercatus/AquaVet/Mobile/PhoneGap/AquaVet.Mobile/platforms/android/bin/classes/no/ocea/aquavet/R$attr.class \
+/Users/oceabergen/Mercatus/AquaVet/Mobile/PhoneGap/AquaVet.Mobile/platforms/android/bin/classes/no/ocea/aquavet/R$drawable.class \
+/Users/oceabergen/Mercatus/AquaVet/Mobile/PhoneGap/AquaVet.Mobile/platforms/android/bin/classes/no/ocea/aquavet/R$string.class \
+/Users/oceabergen/Mercatus/AquaVet/Mobile/PhoneGap/AquaVet.Mobile/platforms/android/bin/classes/no/ocea/aquavet/R$xml.class \
+/Users/oceabergen/Mercatus/AquaVet/Mobile/PhoneGap/AquaVet.Mobile/platforms/android/bin/classes/no/ocea/aquavet/R.class \
 /Users/oceabergen/Mercatus/AquaVet/Mobile/PhoneGap/AquaVet.Mobile/platforms/android/bin/dexedLibs/classes-2f2da8b8793aed473168ac885c84769a.jar \
